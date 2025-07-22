ad478bb74eca930f3d32918de18809b15d3f7739
aakash barasiya <barasia2001@gmail.com>
2025-07-22 13:22:25 +0530

    Add files via upload

--- projects.js
+++ projects.js
@@ -7,3 +7,26 @@ VanillaTilt.init(document.querySelectorAll(".project-box"), {
   gyroscope: true,
   reverse: false
 });
+
+// 3D Tilt Effect
+document.querySelectorAll('.project-card').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((y - centerY) / centerY) * 10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    card.style.setProperty('--rotate-x', `${-rotateX}deg`);
+    card.style.setProperty('--rotate-y', `${rotateY}deg`);
+    card.classList.add('tilt');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.classList.remove('tilt');
+    card.style.setProperty('--rotate-x', `0deg`);
+    card.style.setProperty('--rotate-y', `0deg`);
+  });
+});
